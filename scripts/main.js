219f9bd5903b3abe77ec0ce54b0538c48e67a022
Carly Orr <carly_wong_orr@bcit.ca>
2023-03-01 14:22:23 -0800

    starting point based on demo911

---
+++ scripts/main.js
@@ -0,0 +1,110 @@
+function insertNameFromFirebase(){
+    // to check if the user is logged in:
+     firebase.auth().onAuthStateChanged(user =>{
+         if (user){
+             console.log(user.uid); // let me to know who is the user that logged in to get the UID
+            currentUser = db.collection("users").doc(user.uid); // will to to the firestore and go to the document of the user
+            currentUser.get().then(userDoc=>{
+                //get the user name
+                var user_Name= userDoc.data().name;
+                console.log(user_Name);
+                $("#name-goes-here").text(user_Name); //jquery
+                // document.getElementByID("name-goes-here").innetText=user_Name;
+            })    
+        }
+    
+     })
+    }
+insertNameFromFirebase()
+
+function readQuote() {
+    db.collection("quotes").doc("tuesday")                                                      //name of the collection and documents should matach excatly with what you have in Firestore
+      .onSnapshot(tuesdayDoc => {                                                               //arrow notation
+           console.log("current document data: " + tuesdayDoc.data());                          //.data() returns data object
+           document.getElementById("quote-goes-here").innerHTML = tuesdayDoc.data().quote;      //using javascript to display the data on the right place
+           
+           //Here are other ways to access key:value data fields
+           //$('#quote-goes-here').text(tuesdayDoc.data().quote);                                       //using jquery object dot notation
+           //$("#quote-goes-here").text(tuesdayDoc.data()["quote"]);                                    //using json object indexing
+      })
+}
+readQuote();        //calling the function
+
+function writeHikes() {
+    //define a variable for the collection you want to create in Firestore to populate data
+    var hikesRef = db.collection("hikes");
+
+    hikesRef.add({
+        code: "BBY01",
+        name: "Burnaby Lake Park Trail", //replace with your own city?
+        city: "Burnaby",
+        province: "BC",
+        level: "easy",
+				details: "A lovely place for lunch walk",
+        length: 10,          //number value
+        hike_time: 60,       //number value
+        lat: 49.2467097082573,
+        lng: -122.9187029619698,
+        last_updated: firebase.firestore.FieldValue.serverTimestamp()  //current system time
+    });
+    hikesRef.add({
+        code: "AM01",
+        name: "Buntzen Lake Trail", //replace with your own city?
+        city: "Anmore",
+        province: "BC",
+        level: "moderate",
+        details: "Close to town, and relaxing",
+        length: 10.5,      //number value
+        hike_time: 80,     //number value
+        lat: 49.3399431028579,
+        lng: -122.85908496766939,
+        last_updated: firebase.firestore.Timestamp.fromDate(new Date("March 10, 2022"))
+    });
+    hikesRef.add({
+        code: "NV01",
+        name: "Mount Seymour Trail", //replace with your own city?
+        city: "North Vancouver",
+        province: "BC",
+        level: "hard",
+        details:  "Amazing ski slope views",
+        length: 8.2,        //number value
+        hike_time: 120,     //number value
+        lat: 49.38847101455571,
+        lng: -122.94092543551031,
+        last_updated: firebase.firestore.Timestamp.fromDate(new Date("January 1, 2023"))
+    });
+}
+
+function displayCardsDynamically(collection) {
+    let cardTemplate = document.getElementById("hikeCardTemplate");
+
+    db.collection(collection).get()   //the collection called "hikes"
+        .then(snap => {
+            //var i = 1;  //if you want to use commented out section
+            snap.forEach(doc => { //iterate thru each doc
+                var title = doc.data().name;        // get value of the "name" key
+                var details = doc.data().details;   // get value of the "details" key
+				var hikeCode = doc.data().code;    //get unique ID to each hike to be used for fetching right image
+                var hikeLength = doc.data().length; //gets the length field
+                var docID= doc.id;
+                // var hikeID = doc.data().code;
+                let newcard = cardTemplate.content.cloneNode(true);
+
+                //update title and text and image
+                newcard.querySelector('.card-title').innerHTML = title;
+                newcard.querySelector('.card-length').innerHTML = hikeLength +"km";
+                newcard.querySelector('.card-text').innerHTML = details;
+                // newcard.querySelector('.link-primary').onclick = () => setHikeData(hikeCode)
+                newcard.querySelector('.card-image').src = `./images/${hikeCode}.jpg`; //Example: NV01.jpg
+                newcard.querySelector('.read-more').href = "hike.html?id="+docID; 
+                //attach to gallery, Example: "hikes-go-here"
+                document.getElementById(collection + "-go-here").appendChild(newcard);
+            })
+        })
+}
+
+displayCardsDynamically("hikes");  //input param is the name of the collection
+
+function setHikeData(id){
+    localStorage.setItem ('hikeCode', id);
+}
